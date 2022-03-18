49469d807310d73ed9370536e989c69510b7f17f
Andrew Melo <andrewmelo42@gmail.com>
2022-03-17 23:57:36 -0300

    Fixing for hosting

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ CLIENT_ID = os.getenv("CLIENT_ID") or "No client ID found. Check the .env file."
 GUILD_ID = os.getenv("GUILD_ID") or "No guild ID found. Check the .env file."
 
 #DATABASE CONNECTION
-ENGINE_URL = os.getenv("ENGINE_URL") or "Invalid URL"
+ENGINE_URL = os.getenv("JAWSDB_URL") or "Invalid URL"
